e6a165d2e723f321efe57ed02eff5655a74ebcc9
Mehul Kothari <34826782+mehulk05@users.noreply.github.com>
2020-10-12 16:30:08 +0530

    Updates

---
+++ precache-manifest.ee788a6420a35e9ba9f12581195e589f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42cec1dbff21baff9930ebb279789075",
+    "url": "/Blogapp-using-MERN/index.html"
+  },
+  {
+    "revision": "73fa47d6bcf55f8aaded",
+    "url": "/Blogapp-using-MERN/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "514f6cf5e7960d6f105c",
+    "url": "/Blogapp-using-MERN/static/css/main.a1d0157e.chunk.css"
+  },
+  {
+    "revision": "73fa47d6bcf55f8aaded",
+    "url": "/Blogapp-using-MERN/static/js/2.81d386fd.chunk.js"
+  },
+  {
+    "revision": "10300dfa896f6ed2c6255ffefdb0373b",
+    "url": "/Blogapp-using-MERN/static/js/2.81d386fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "514f6cf5e7960d6f105c",
+    "url": "/Blogapp-using-MERN/static/js/main.d269d863.chunk.js"
+  },
+  {
+    "revision": "0044862a76ac19084d9d",
+    "url": "/Blogapp-using-MERN/static/js/runtime-main.adcc9ebc.js"
+  },
+  {
+    "revision": "171cd5d91c86399e33eaa18c2a53a9c0",
+    "url": "/Blogapp-using-MERN/static/media/asset-1.171cd5d9.png"
+  }
+]);
